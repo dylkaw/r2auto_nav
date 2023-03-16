4ef190106361644a1df662693edb782517542fb1
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-03-16 20:09:30 +0800

    added quarternion to euler conversion

--- waypoints.py
+++ waypoints.py
@@ -36,6 +36,29 @@ scanfile = 'lidar.txt'
 mapfile = 'map.txt'
 waypoints = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
 
+# code from https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z # in radians
+    
 class Waypoint(Node):
 
     def __init__(self):
@@ -63,15 +86,12 @@ class Waypoint(Node):
             orien =  msg.pose.pose.orientation
             px = msg.pose.pose.position.x
             py = msg.pose.pose.position.y
-            ox = orien.x
-            oy = orien.y
-            oz = orien.z
-            ow = orien.w
+            ox, oy, oz = euler_from_quaternion(orien.x, orien.y, orien.z, orien.w)
             # self.get_logger().info(orien)
             while numbers != 0:
                 num = numbers % 10
                 numbers = (numbers // 10)
-                data = (px, py, ox, oy, oz, ow)
+                data = (px, py, ox, oy, oz)
                 waypoints[num].append(data)
             print(waypoints)
 
